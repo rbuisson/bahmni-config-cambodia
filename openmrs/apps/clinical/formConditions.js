ca134082ceab01b27d67e30a1c6bf1bbd0788a97
mksrom <romain@mekomsolutions.com>
2017-05-02 17:32:16 +0200

    BAHMNI-20: Conditionally display obs sets based on patient gender
    	modified:   openmrs/apps/clinical/app.json
    	modified:   openmrs/apps/clinical/extension.json
    	modified:   openmrs/apps/clinical/formConditions.js

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -46,7 +46,6 @@ Bahmni.ConceptSet.FormConditions.rules = {
         }
 
         return sections
-
     },
     'CAMBODIA_Immunizations complete' : function (formName, formFieldValues) {
         var fieldValue = formFieldValues['CAMBODIA_Immunizations complete'];
@@ -59,7 +58,6 @@ Bahmni.ConceptSet.FormConditions.rules = {
                 hide: ["CAMBODIA_List incomplete immunizations"]
             }
         }
-
     },
     'CAMBODIA_Medications' : function (formName, formFieldValues) {
         var fieldValue = formFieldValues['CAMBODIA_Medications'];
@@ -72,7 +70,6 @@ Bahmni.ConceptSet.FormConditions.rules = {
                 hide: ["CAMBODIA_List medications"]
             }
         }
-
     },
     'CAMBODIA_Allergies' : function (formName, formFieldValues) {
         var fieldValue = formFieldValues['CAMBODIA_Allergies'];
@@ -85,7 +82,26 @@ Bahmni.ConceptSet.FormConditions.rules = {
                 hide: ["CAMBODIA_List allergies"]
             }
         }
+    },
+    'CAMBODIA_Genitourinary Findings Set' : function (formName, formFieldValues, patient) {
+        var patientGender = patient['gender'];
+        var sections = {
+            "show": [],
+            "hide": []
+        }
 
+        if (patientGender == 'M') {
+            sections.show.push("CAMBODIA_Genitourinary Male Findings")
+            sections.hide.push("CAMBODIA_Genitourinary Female Findings")
+        } else if (patientGender == 'F') {
+            sections.hide.push("CAMBODIA_Genitourinary Male Findings")
+            sections.show.push("CAMBODIA_Genitourinary Female Findings")
+        } else {
+            sections.show.push("CAMBODIA_Genitourinary Male Findings")
+            sections.show.push("CAMBODIA_Genitourinary Female Findings")            
+        }
+
+        return sections
     },
     'CAMBODIA_Skin Exam' : function (formName, formFieldValues) {
         var fieldValue = formFieldValues['CAMBODIA_Skin Exam'];
@@ -98,7 +114,6 @@ Bahmni.ConceptSet.FormConditions.rules = {
                 disable: ["CAMBODIA_Skin Exam Abnormalities", "CAMBODIA_Skin Exam Details"]
             }
         }
-
     },
     'CAMBODIA_Head Exam' : function (formName, formFieldValues) {
         var fieldValue = formFieldValues['CAMBODIA_Head Exam'];
@@ -111,7 +126,6 @@ Bahmni.ConceptSet.FormConditions.rules = {
                 disable: ["CAMBODIA_Head Exam Details"]
             }
         }
-
     },
     'CAMBODIA_Neck Exam' : function (formName, formFieldValues) {
         var fieldValue = formFieldValues['CAMBODIA_Neck Exam'];
@@ -124,7 +138,6 @@ Bahmni.ConceptSet.FormConditions.rules = {
                 disable: ["CAMBODIA_Neck Exam Details", "CAMBODIA_Neck Exam Abnormalities"]
             }
         }
-
     },
     'CAMBODIA_Ears Exam' : function (formName, formFieldValues) {
         var fieldValue = formFieldValues['CAMBODIA_Ears Exam'];
@@ -137,7 +150,6 @@ Bahmni.ConceptSet.FormConditions.rules = {
                 disable: ["CAMBODIA_Ears Exam Details"]
             }
         }
-
     },
     'CAMBODIA_Eyes Exam' : function (formName, formFieldValues) {
         var fieldValue = formFieldValues['CAMBODIA_Eyes Exam'];
@@ -150,7 +162,6 @@ Bahmni.ConceptSet.FormConditions.rules = {
                 disable: ["CAMBODIA_Eyes Exam Details", "CAMBODIA_Eyes Exam Abnormalities"]
             }
         }
-
     },
     'CAMBODIA_Chest and Lungs Exam' : function (formName, formFieldValues) {
         var fieldValue = formFieldValues['CAMBODIA_Chest and Lungs Exam'];
@@ -163,7 +174,6 @@ Bahmni.ConceptSet.FormConditions.rules = {
                 disable: ["CAMBODIA_Chest and Lungs Exam Details", "CAMBODIA_Chest and Lungs Exam Abnormalities"]
             }
         }
-
     },
     'CAMBODIA_Cardiovascular Exam' : function (formName, formFieldValues) {
         var fieldValue = formFieldValues['CAMBODIA_Cardiovascular Exam'];
@@ -176,7 +186,6 @@ Bahmni.ConceptSet.FormConditions.rules = {
                 disable: ["CAMBODIA_Cardiovascular Exam Details and Other", "CAMBODIA_Cardiovascular Exam Abnormalities"]
             }
         }
-
     },
     'CAMBODIA_Cardiovascular Exam Abnormalities' : function (formName, formFieldValues) {
         var fieldValue = formFieldValues['CAMBODIA_Cardiovascular Exam Abnormalities'];
@@ -211,7 +220,6 @@ Bahmni.ConceptSet.FormConditions.rules = {
                 disable: ["CAMBODIA_Abdomen Exam Details", "CAMBODIA_Abdomen Exam Abnormalities"]
             }
         }
-
     },
     'CAMBODIA_Flank and Back Exam' : function (formName, formFieldValues) {
         var fieldValue = formFieldValues['CAMBODIA_Flank and Back Exam'];
@@ -224,20 +232,42 @@ Bahmni.ConceptSet.FormConditions.rules = {
                 disable: ["CAMBODIA_Flank and Back Exam Details"]
             }
         }
-
     },
-    'CAMBODIA_Genitourinary Exam' : function (formName, formFieldValues) {
+    'CAMBODIA_Genitourinary Exam' : function (formName, formFieldValues, patient) {
         var fieldValue = formFieldValues['CAMBODIA_Genitourinary Exam'];
-        if (fieldValue == "CAMBODIA_Abnormal") {
-            return {
-                enable: ["CAMBODIA_Genitourinary Exam Details", "CAMBODIA_Genitourinary Exam Abnormalities"]
-            }
-        } else {
-            return {
-                disable: ["CAMBODIA_Genitourinary Exam Details", "CAMBODIA_Genitourinary Exam Abnormalities"]
-            }
+        var patientGender = patient['gender'];
+        var sections = {
+            "show": [],
+            "hide": [],
+            "enable": [],
+            "disable": []
         }
 
+        // if (patientGender == 'M') {
+        //     sections.show.push("CAMBODIA_Genitourinary Male Exam Abnormalities")
+        //     sections.hide.push("CAMBODIA_Genitourinary Female Exam Abnormalities")
+        // } else if (patientGender == 'F') {
+        //     sections.hide.push("CAMBODIA_Genitourinary Male Exam Abnormalities")
+        //     sections.show.push("CAMBODIA_Genitourinary Female Exam Abnormalities")
+        // } else {
+        //     sections.show.push("CAMBODIA_Genitourinary Male Exam Abnormalities")
+        //     sections.show.push("CAMBODIA_Genitourinary Female Exam Abnormalities")            
+        // }
+
+        if (fieldValue == "CAMBODIA_Abnormal") {
+            sections.enable.push("CAMBODIA_Genitourinary Exam Details")
+            sections.enable.push("CAMBODIA_Genitourinary Male Exam Abnormalities")
+            sections.enable.push("CAMBODIA_Genitourinary Female Exam Abnormalities")
+            if (patientGender == 'M') sections.hide.push("CAMBODIA_Genitourinary Female Exam Abnormalities")
+        } else {
+            sections.disable.push("CAMBODIA_Genitourinary Exam Details")
+            sections.disable.push("CAMBODIA_Genitourinary Male Exam Abnormalities")
+            sections.disable.push("CAMBODIA_Genitourinary Female Exam Abnormalities")
+            if (patientGender == 'M') sections.hide.push("CAMBODIA_Genitourinary Female Exam Abnormalities")
+            
+        }
+        
+        return sections
     },
     'CAMBODIA_Musculoskeletal and Neurological Exam' : function (formName, formFieldValues) {
         var fieldValue = formFieldValues['CAMBODIA_Musculoskeletal and Neurological Exam'];
